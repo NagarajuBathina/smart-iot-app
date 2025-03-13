5785412d22086340559e7e87aaa9e3782da0f790
Naga raju <113161518+NagarajuBathina@users.noreply.github.com>
2025-03-13 19:06:42 +0530

    last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_iot_app/provider.dart';
 import 'package:smart_iot_app/screens/home_screen.dart';
@@ -7,6 +8,7 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(MultiProvider(
